68a857b84a04da82ba715c396f918d849e88e5ef
MangGlang <71730799+MangGlang@users.noreply.github.com>
2023-12-23 23:26:56 -0800

    Implemented Recommended/Suggested responsive carousel

--- components/SuggestedBooks.jsx
+++ components/SuggestedBooks.jsx
@@ -22,36 +22,38 @@ const SuggestedBooks = () => {
     <div className="row">
       <div className="container">
         <div className="for-you__tile">Suggested For You</div>
-        <p className="font-thin text-[#394547] text-base font-sans">
+        <p className="font-thin text-[#394547] text-[17px] text-base font-sans">
           Browse those books
         </p>
-        <div className="book__container flex overflow-scroll">
+        <div className="book__container flex overflow-scroll snap-x">
           {suggestedBooks &&
             suggestedBooks.map((books) => (
-              <div className="flex flex-col">
-                <img
-                  src={books.imageLink}
-                  className="mx-4 w-[11rem] max-w-none"
-                ></img>
-                <div className="mx-4">
-                  <div>
-                    <h1 className="tracking-tight font-bold text-[#032b41] leading-5 font-sans">
-                      {books.title}
-                    </h1>
-                    <p className="text-sm my-1 font-sans font-thin text-[#394547]">
-                      {books.author}
-                    </p>
-                    <p className="text-sm my-1 font-sans text-[#394547]">
-                      {books.subTitle}
-                    </p>
-                    <div className="flex items-center text-[#6b757b]">
-                      <LuClock2 className="mr-1" />
-                      <p className="text-sm">{`00:00`}</p>{" "}
-                      <LuStar className="ml-2" />{" "}
-                      <p className="text-sm ml-1">{books.averageRating}</p>
+              <div className="flex flex-col snap-start">
+                <a href={`/book/${books.id}`} className="pt-8 hover:bg-[#f1f6f4] w-full h-full">
+                  <img
+                    src={books.imageLink}
+                    className="mx-4 w-[11rem] max-w-none"
+                  ></img>
+                  <div className="mx-4">
+                    <div>
+                      <h1 className="tracking-tight font-bold text-[#032b41] leading-5 font-sans">
+                        {books.title}
+                      </h1>
+                      <p className="text-sm my-1 font-sans font-thin text-[#394547]">
+                        {books.author}
+                      </p>
+                      <p className="text-sm my-1 font-sans text-[#394547]">
+                        {books.subTitle}
+                      </p>
+                      <div className="flex items-center text-[#6b757b]">
+                        <LuClock2 className="mr-1" />
+                        <p className="text-sm">{`00:00`}</p>{" "}
+                        <LuStar className="ml-2" />{" "}
+                        <p className="text-sm ml-1">{books.averageRating}</p>
+                      </div>
                     </div>
                   </div>
-                </div>
+                </a>
               </div>
             ))}
         </div>
