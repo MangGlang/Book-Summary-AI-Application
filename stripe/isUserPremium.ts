f5bbff0ccf10cf3a391e2338341cd3d855ea7342
MangGlang <71730799+MangGlang@users.noreply.github.com>
2024-01-13 19:08:18 -0800

    edit: deleted conflicting file

--- stripe/isUserPremium.ts
+++
@@ -1,9 +0,0 @@
-import firebase from "@/firebase";
-
-export default async function isUserPremium(): Promise<boolean> {
-    await firebase.auth().currentUser?.getIdToken(true);
-    const decodedToken = await firebase.auth().currentUser?.getIdTokenResult();
-
-    return decodedToken?.claims?.stripeRole ? true : false;
-
-}
