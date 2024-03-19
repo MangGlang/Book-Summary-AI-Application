ffb145ee8ff6032084eb3aee9f3c1ea3855c608d
MangGlang <71730799+MangGlang@users.noreply.github.com>
2024-03-19 09:44:40 -0700

    exported

--- firebase.jsx
+++ firebase.jsx
@@ -11,7 +11,7 @@ import { createUserWithEmailAndPassword } from "firebase/auth";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-const firebaseConfig = {
+export const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
